2b39b7b64dc149877fef0674dad2647c794f4ba0
cayden <cuiran2001@163.com>
2013-09-18 21:49:33 +0800

    1111

--- src/com/example/bloothdemo/MainActivity.java
+++ src/com/example/bloothdemo/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        
         main_img=(TextView)findViewById(R.id.main_img);
         
         main_img.setOnClickListener(this);
